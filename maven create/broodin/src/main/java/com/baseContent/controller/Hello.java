5cc4a16e1fc62aab52c036fcf42670ddc8235155
darren <darren@darren-PC>
2017-01-22 14:55:49 +0800

    add base department br open project

---
+++ maven create/broodin/src/main/java/com/baseContent/controller/Hello.java
@@ -0,0 +1,13 @@
+package com.baseContent.controller;
+import org.springframework.stereotype.Controller;  
+import org.springframework.ui.Model;  
+import org.springframework.web.bind.annotation.RequestMapping;  
+@Controller 
+public class Hello {
+	@RequestMapping(value="/Hello")  
+    public String HelloWorld(Model model){  
+        model.addAttribute("message","Hello World!!!");  
+        return "HelloWorld";  
+    }  
+}
+	
